e2e63612f7ec9e389c6fa572e014b0dc6af72994
edgarizquierdo <edgar@arcadiaclimatic.com>
2026-01-02 01:40:18 +0100

    Update CsvUpload.jsx

--- src/CsvUpload.jsx
+++ src/CsvUpload.jsx
@@ -83,7 +83,7 @@ export default function CsvUpload({ onData }) {
           width: "100%",
           background: "#3563E9",
           color: "white",
-          padding: "8px 9px",
+          padding: "8px 4px",
           borderRadius: 10,
           cursor: "pointer",
           textAlign: "center",
